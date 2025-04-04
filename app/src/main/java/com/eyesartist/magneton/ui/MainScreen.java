f545a8dd94901a36b6f4b4cddaf2a8755b20c2d9
Huy <huycachuoi@gmail.com>
2025-04-04 19:27:58 +0700

    up lan 2 trong ngay de sua nav

--- app/src/main/java/com/eyesartist/magneton/ui/MainScreen.java
+++ app/src/main/java/com/eyesartist/magneton/ui/MainScreen.java
@@ -16,6 +16,6 @@ public class MainScreen extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.blankfragment,container,false);
+        return inflater.inflate(R.layout.mainview,container,false);
     }
 }
