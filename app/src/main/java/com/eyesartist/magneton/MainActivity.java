02f4335ab85c1af1ef2301ab0df7ca5bd98abe8d
Huy <huycachuoi@gmail.com>
2025-04-12 22:25:51 +0700

    tiep tuc sua lai giao dien

--- app/src/main/java/com/eyesartist/magneton/MainActivity.java
+++ app/src/main/java/com/eyesartist/magneton/MainActivity.java
@@ -1,6 +1,8 @@
 package com.eyesartist.magneton;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,15 +21,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        //appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        View v = new View(this);
+        makeStatusBarTransparent();
     }
-/*
+    private void makeStatusBarTransparent() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+    }
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
- */
 }
--- app/src/main/java/com/eyesartist/magneton/ui/MainScreen.java
+++ app/src/main/java/com/eyesartist/magneton/ui/MainScreen.java
@@ -8,14 +8,23 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.eyesartist.magneton.R;
 
 public class MainScreen extends Fragment {
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.mainview,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        NavController navController = Navigation.findNavController(view);
+
     }
 }
