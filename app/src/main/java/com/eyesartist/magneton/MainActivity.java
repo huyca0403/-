c556fcaef843351ff51efb4bb13002ae0a5e1538
Huy <huycachuoi@gmail.com>
2025-04-12 23:06:34 +0700

    sua xong giao dien r, nma chx co code java

--- app/src/main/java/com/eyesartist/magneton/MainActivity.java
+++ app/src/main/java/com/eyesartist/magneton/MainActivity.java
@@ -21,15 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        View v = new View(this);
-        makeStatusBarTransparent();
     }
-    private void makeStatusBarTransparent() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        );
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
-    }
+
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
