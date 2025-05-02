b92393265a69405a463c533fa3b747cf9735f633
Huy <huycachuoi@gmail.com>
2025-05-02 20:19:37 +0700

    xong phan chuyen doi r

--- app/src/main/java/com/eyesartist/magneton/ui/MainScreen.java
+++ app/src/main/java/com/eyesartist/magneton/ui/MainScreen.java
@@ -1,10 +1,14 @@
 package com.eyesartist.magneton.ui;
 
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -16,15 +20,39 @@ import androidx.navigation.ui.NavigationUI;
 import com.eyesartist.magneton.R;
 
 public class MainScreen extends Fragment {
+    private long pressedTime;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.mainview,container,false);
+        
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                //không có gì
+                Log.d("tag","k cho ra");
+                if(pressedTime+2000> System.currentTimeMillis()){
+                    requireActivity().finishAffinity();
+                }else {
+                    Toast.makeText(getContext(),"Không cho ra",Toast.LENGTH_LONG).show();
+                    Log.d("lol", String.valueOf(pressedTime));
+                    pressedTime = System.currentTimeMillis();
+                }
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(this,callback);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         NavController navController = Navigation.findNavController(view);
-
+        view.findViewById(R.id.taskbar_left_icon).setOnClickListener(v->{
+            navController.navigate(R.id.action_mainScreen_to_secondaryScreen);
+        });
     }
 }
--- app/src/main/java/com/eyesartist/magneton/ui/SecondaryScreen.java
+++ app/src/main/java/com/eyesartist/magneton/ui/SecondaryScreen.java
@@ -5,18 +5,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.eyesartist.magneton.R;
 
 public class SecondaryScreen extends Fragment {
-
+    NavController navController;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_2,container,false);
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                //không có gì
+                navController.popBackStack();
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(this,callback);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        this.navController = Navigation.findNavController(view);
+        //quay lại bằng việc bấm
+        view.findViewById(R.id.cancel_button).setOnClickListener(v->{
+            this.navController.popBackStack();
+        });
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
